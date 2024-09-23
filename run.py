3c21e7463f937439d8cd13d3527884bef7cad5ac
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-09-23 09:10:20 +0000

    Add functionality to display the to-do list

--- run.py
+++ run.py
@@ -7,3 +7,19 @@ def add_task(task):
     to_do_list.append({'task': task, 'done': False})
     print(f'Task {task} added successfully!\n')
 
+def show_tasks():
+    """
+    Displays the list of tasks with their current status.
+    If no tasks are available, it notifies the user.
+    """
+    if not to_do_list:
+        print("Your to-do list is emtpy!/n")
+    else:
+        print("Your to do list:")
+        for idx, task in enumerate(to_do_list):
+            status = "Done" if task['done'] else "Pending"
+            print(f"{idx +1}. {task['task']} - {status}")
+        print()
+
+add_task("Go to the gym")
+show_tasks()
