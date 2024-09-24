7d9827719ca80cba2536f267fd6eb346d64a37ed
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-09-24 10:09:37 +0000

    Allow repeated attempts to remove or mark tasks as done when input is invalid

--- run.py
+++ run.py
@@ -85,19 +85,35 @@ def main():
 
         elif choice == "2":
             show_tasks()
-            try:
-                task_num = int(input("Enter the task number to remove:\n"))-1
-                remove_task(task_num)
-            except ValueError:
-                print("Please enter a valid number.\n")
+            while True:
+                show_tasks()
+                if not to_do_list:
+                    print("No tasks to remove.\n")
+                    break
+                try:
+                    task_num = int(input("Enter the task number to remove:\n"))-1
+                    remove_task(task_num)
+                    break
+                except ValueError:
+                    print("Please enter a valid number.\n")
+                except IndexError:
+                    print("Invalid task number. Please try again!\n")
 
         elif choice == "3":
             show_tasks()
-            try:
-                task_num = int(input("Enter task number to mark as done:\n"))-1
-                mark_task_done(task_num)
-            except ValueError:
-                print("Please enter a valid number.\n")
+            while True:
+                show_tasks()
+                if not to_do_list:
+                    print("No tasks to mark as done.\n")
+                    break
+                try:
+                    task_num = int(input("Enter task number to mark as done:\n"))-1
+                    mark_task_done(task_num)
+                    break
+                except ValueError:
+                    print("Please enter a valid number.\n")
+                except IndexError:
+                    print("Invalid task number. Please try again!\n")
 
         elif choice == "4":
             show_tasks()
